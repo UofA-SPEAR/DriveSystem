18c01e2bfdf5f10feebf13a45d215edf6c5c24f8
Spear <ofrim@ualberta.ca>
2018-01-16 11:28:44 -0700

    setup serial and isr

--- DriveSystem/main.c
+++ DriveSystem/main.c
@@ -6,13 +6,41 @@
  */ 
 
 #include <avr/io.h>
+#include<avr/interrupt.h>
+#include "uart.h"
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	//Initialize serial coms
+	stdout = &uart_output;
+	uart_init();
+		
+
+	// Set the Timer Mode to CTC
+	TCCR0A |= (1 << WGM01);
+
+	// Set the value that you want to count to
+	OCR0A = 0xFFFF;
+
+	TIMSK0 |= (1 << OCIE0A);    //Set the ISR COMPA vect
+
+	sei();         //enable interrupts
+
+
+	TCCR0B |= (1 << CS02) | (1 << CS00);
+	// set prescaler to 1024 and start the timer
+
+
+	while (1)
+	{
+		//main loop
+	}
+}
+
+ISR (TIMER0_COMPA_vect)  // timer0 overflow interrupt
+{
+	//event to be exicuted every 4ms here
+	printf("ISR count: %i\n", isrCout ++);
 }
 
---
+++ DriveSystem/uart.h
@@ -0,0 +1,13 @@
+/* Code was taken from:
+* https://github.com/tuupola/avr_demo/blob/master/blog/simple_usart/uart.h
+*/
+#include <stdio.h>
+void uart_putchar(char c, FILE *stream);
+char uart_getchar(FILE *stream);
+
+void uart_init();
+
+/* http://www.ermicro.com/blog/?p=325 */
+
+FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
