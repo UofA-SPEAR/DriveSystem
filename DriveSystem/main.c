9e0baa498742e2f30f106bc9019b1c1d91bccfce
Spear <ofrim@ualberta.ca>
2018-01-16 13:52:40 -0700

    blinking light

--- DriveSystem/main.c
+++ DriveSystem/main.c
@@ -1,91 +1,52 @@
-/*
- * DriveSystem.c
- *
- * Created: 2018-01-16 11:16:23 AM
- * Author : spear
- */ 
-#define F_CPU 8000000UL
+///*
+ //* DriveSystem.c
+ //*
+ //* Created: 2018-01-16 11:16:23 AM
+ //* Author : spear
+ //*/ 
 
+#define F_CPU 16000000UL
 #include <avr/io.h>
-#include<avr/interrupt.h>
-#include "uart.h"
 
-int super_loop_flag = 0;
-int isr_count = 0;
+#include <avr/interrupt.h> // notice that we have swapped libraries, from delay to interrupt
+#include <util/delay.h>
+#include <stdbool.h>
 
-// function definitions 
-void super_loop_init();
-void output_pins_init();
-void toggle_LED13();
+volatile bool flag = false;
 
-int main(void)
-{
-	//Initialize serial coms
-	stdout = &uart_output;
-	uart_init();
+int main (void) {
 
-	// set up the super loop isr
-	super_loop_init();
+	//// set as output pins
+	DDRB |= _BV(DDB5);
+	PORTB |= _BV(PORTB5);
+
 	
-	output_pins_init();
 
-	while (1) //main loop
-	{
-		printf("Start main loop\n");		
-		while(super_loop_flag == 0)
-		{
-			//printf("Waiting for loop\n");
-		}
-		//printf("flag set\n");
-		isr_count++;
-		isr_count = isr_count % 256;
-		//printf("isr_count %i\n", isr_count);
-		if(isr_count == 0){
-			toggle_LED13();
-		}
-		// read any incoming data
-		
-		// calculate motor signals
-		
-		// read motor feedback
-		super_loop_flag = 0;
+	TCCR1B |= (1 << WGM12); // configure timer1 for CTC mode
+
+	TIMSK1 |= (1 << OCIE1A); // enable the CTC interrupt b 
+
+	sei(); // enable global interrupts
+
+	OCR1A   = 19531/10; // set the CTC compare value
+
+	TCCR1B |= ((1 << CS10) | (1 << CS12)); // start the timer at 20MHz/1024
+
+	while(1) { // main loop - do anything you like here!
+		while(flag == false);
+		PORTB ^= _BV(PORTB5); // toggle the LED
+		flag = false;
 	}
+
 }
 
-ISR (TIMER0_COMPA_vect)  // timer0 overflow interrupt
-{
-	//printf("Service Routine hit\n");
-	if(super_loop_flag == 1){
-		//printf("loop time too short\n");
-	}
-	super_loop_flag = 1;
-	//event to be exicuted every 4ms here
-	//printf("ISR count: %i\n", isrCout ++);
-}
 
-void super_loop_init(){
-	// Set the Timer Mode to CTC
-	TCCR0A |= (1 << WGM01);
 
-	// Set the value that you want to count to
-	OCR0A = 0xFFFF;
+ISR(TIMER1_COMPA_vect) { // this function is called every time the timer reaches the threshold we set
 
-	TIMSK0 |= (1 << OCIE0A);    //Set the ISR COMPA vect
+	
+	flag = true;
 
-	sei();         //enable interrupts
-	TCCR0B |= (1 << CS02) | (1 << CS00);
-	// set prescaler to 1024 and start the timer
-}
-
-void output_pins_init(){
-	// set as output pins
-	DDRB |= _BV(DDB3);
-	PORTB |= _BV(PORTB3);
-}
-
-void toggle_LED13(){
-	printf("toggle led");
-	PORTB ^= _BV(PORTB3);
 }
 
 
