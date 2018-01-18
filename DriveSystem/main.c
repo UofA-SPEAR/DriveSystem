abf1ebff2b767198a9681de766d2a7dad8d63ecb
Brad Ofrim <ofrim@ualberta.ca>
2018-01-18 12:46:45 -0800

    Adds clock freq defines

--- DriveSystem/main.c
+++ DriveSystem/main.c
@@ -13,6 +13,11 @@
 #define F_CPU 16000000UL
 #endif
 
+#define F_CLK_1024 F_CPU / 1024UL
+#define F_CLK_256 F_CPU / 256UL
+#define F_CLK_64 F_CPU / 64UL
+#define F_CLK_8 F_CPU / 8UL
+
 volatile bool usart_rx_flag = false;
 
 void setup_pins();
