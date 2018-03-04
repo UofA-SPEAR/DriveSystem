65de63b7f5afda7b5bfbe48f268096d21e3d47fe
Spear <ofrim@ualberta.ca>
2018-03-04 10:51:40 -0700

    Adds motor control files

---
+++ DriveSystem/motor_control.c
@@ -0,0 +1,6 @@
+/*
+ * motor_control.c
+ *
+ * Created: 2018-03-04 10:51:20 AM
+ *  Author: spear
+ */ 
---
+++ DriveSystem/motor_control.h
@@ -0,0 +1,16 @@
+/*
+ * motor_control.h
+ *
+ * Created: 2018-03-04 10:47:16 AM
+ *  Author: spear
+ */ 
+
+
+#ifndef MOTOR_CONTROL_H_
+#define MOTOR_CONTROL_H_
+
+
+
+
+
+#endif /* MOTOR_CONTROL_H_ */
