8d4dc0d4bc3ebfc04049bcd207d4282035cd108d
Spear <ofrim@ualberta.ca>
2018-03-04 16:48:02 -0700

    Adds I/O pin direction ports

--- DriveSystem/hardware_config.h
+++ DriveSystem/hardware_config.h
@@ -12,6 +12,19 @@
 /* Motor Controls */
 
 /*
+ * Configure pin I/O direction registers and pins
+ * Used for setting input or output direction of hardware pins
+ */
+#define LEFT_PWM_IO_REG				DDRB
+#define LEFT_PWM_IO_PIN				DDB6
+#define LEFT_DIR_IO_REG				DDRB
+#define LEFT_DIR_IO_PIN				DDB4
+#define RIGHT_PWM_IO_REG			DDRB
+#define RIGHT_PWM_IO_PIN			DDB5
+#define RIGHT_DIR_IO_REG			DDRB
+#define RIGHT_DIR_IO_PIN			DDB7
+
+/*
  * Configure registers used for controlling motor direction
  */
 #define LEFT_DIR_REG			PORTB
@@ -28,6 +41,8 @@
  * These registers are the output compare registers used in PWM signals
  */
 #define LEFT_PWM_REG			OCR1B
+#define LEFT_PWM_PIN			PORTB6
 #define RIGHT_PWM_REG			OCR1A
+#define RIGHT_PWM_PIN			PORTB5
 
 #endif /* HARDWARE_CONFIG_H_ */
