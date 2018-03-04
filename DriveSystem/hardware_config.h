6b1ecdaa86f213bafd532f2e8fea0718c8799c44
Spear <ofrim@ualberta.ca>
2018-03-04 16:17:12 -0700

    Adds the defines for motor PWM and direction registers and pins

--- DriveSystem/hardware_config.h
+++ DriveSystem/hardware_config.h
@@ -9,8 +9,23 @@
 #ifndef HARDWARE_CONFIG_H_
 #define HARDWARE_CONFIG_H_
 
+/*
+ * Configure registers used for controlling motor direction
+ */
+#define LEFT_DIR_REG			PORTB
+#define RIGHT_DIR_REG			PORTB
 
+/*
+ * Configure pins used for controlling motor direction
+ */
+#define LEFT_DIR_PIN			PORTB4
+#define RIGHT_DIR_PIN			PORTB7
 
-
+/*
+ * Configure register used for controlling motor speed
+ * These registers are the output compare registers used in PWM signals
+ */
+#define LEFT_PWM_REG			OCR1B
+#define RIGHT_PWM_REG			OCR1A
 
 #endif /* HARDWARE_CONFIG_H_ */
