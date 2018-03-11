5ed31867333f751127cbb25d8e36bb6586023eb9
Spear <ofrim@ualberta.ca>
2018-03-11 10:19:36 -0600

    Adds timer files

---
+++ DriveSystem/timer.c
@@ -0,0 +1,6 @@
+/*
+ * timer.c
+ *
+ * Created: 2018-03-11 10:17:48 AM
+ *  Author: spear
+ */ 
---
+++ DriveSystem/timer.h
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ * Created: 2018-03-11 10:16:26 AM
+ *  Author: spear
+ */ 
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+
+
+
+
+#endif /* TIMER_H_ */
